8f5463bc81195a10a638d1b9210eb358be1e00e9
Paweł <pawelokrasa198@gmail.com>
2018-11-25 03:20:34 +0100

    User

--- CatchUppApi/Models/User.cs
+++ CatchUppApi/Models/User.cs
@@ -2,6 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
+
 namespace CatchUppApi.Models
 {
     public class User :BaseEntity
